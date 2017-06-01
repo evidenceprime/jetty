e35b0a1301384fdefb3ed7dc30c98f4be1cf1028
Grzegorz Stuczynski <stuczynski.g@gmail.com>
2017-06-01 16:34:04 +0200

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "admin@evidenceprime.com"
 license          "Apache 2.0"
 description      "Installs/Configures Jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.11.0"
+version          "0.11.1"
 supports         "debian", ">= 7"
 depends          "java", ">= 1.10.0"
